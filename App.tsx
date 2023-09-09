16e78e8700e2a132cf2bc8f6556b8a6eb74e0a15
nain93 <rnrb555@gmail.com>
2023-09-09 22:16:33 +0900

    feat: add safearea

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
 import GlobalNav from '~/navigators/GlobalNav';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <GlobalNav />
-    </NavigationContainer>
+    <SafeAreaProvider style={styles.safeAreaView}>
+      <NavigationContainer>
+        <GlobalNav />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+});
 
 export default App;
